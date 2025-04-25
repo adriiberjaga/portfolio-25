d0eb8ff13bf3c6b3bef080db31e9c70f29087420
adriiberjaga <adriabdd@gmail.com>
2025-04-25 13:38:45 +0200

    add fonts and header

--- src/pages/ts/Header.tsx
+++ src/pages/ts/Header.tsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import '../css/Header.css'
+import "../css/Header.css";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <header className="header">
       <div className="header-border">
-      <h2 className="header-title">Adrià Berjaga</h2>
-      <nav className="header-navbar">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/">About me</NavLink>
-        <NavLink to="/">Projects</NavLink>
-        <NavLink to="/">Contact</NavLink>
-      </nav>
+        <h2 className="header-title">Adrià Berjaga</h2>
+        
+        {!menuOpen ? (
+          <button type="button" className="hamburger" onClick={toggleMenu}>
+            &#9776;
+          </button>
+        ) : (
+          <button onClick={toggleMenu} className="header-btn__close">
+            &#10005;
+          </button>
+        )}
+
+        <nav className={`header-navbar ${menuOpen ? "open" : ""}`}>
+          <NavLink to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </NavLink>
+          <NavLink to="/about" onClick={() => setMenuOpen(false)}>
+            About me
+          </NavLink>
+          <NavLink to="/projects" onClick={() => setMenuOpen(false)}>
+            Projects
+          </NavLink>
+          <NavLink to="/contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </NavLink>
+        </nav>
       </div>
     </header>
   );
