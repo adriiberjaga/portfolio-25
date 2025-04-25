fb05122c387751299fef0f1d9ccc4f780ab7c013
adriiberjaga <adriabdd@gmail.com>
2025-04-25 13:46:02 +0200

    .

---
+++ src/pages/ts/Home.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+    <main>
+        {/* titulo y texto div izquierda, y imagen derecha, en movil, div izquierda delante y div derecha detras, con la imagen con una opacity  */}
+        <div>
+            <h1>Frontend <br />developer</h1>
+            <p>Soy Adrià Berjaga</p>
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+
+    </main>
+  )
+}
